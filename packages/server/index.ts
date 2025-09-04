fe1945cbefe66bab58504b724f2d5256f84e05ce
John muller <phychicjosh@gmail.com>
2025-09-04 21:26:03 +0300

    connected the backend and frontend

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,12 +1,15 @@
-import express from 'express';
-import type { Request, Response } from 'express';
-import dotenv from 'dotenv'
+import express from "express";
+import type { Request, Response } from "express";
+import dotenv from "dotenv";
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req: Request, res: Response) => res.send("Hello world!"));
+app.get("/", (req: Request, res: Response) => res.send("Hello world!"));
+app.get("/api/hello", (req: Request, res: Response) =>
+  res.send({ message: "Hello, World!" }),
+);
 
 app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
+  console.log(`Server is running at http://localhost:${port}`);
 });
