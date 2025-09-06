66c606e57aca055abf126324d8e2d262618934ff
John muller <phychicjosh@gmail.com>
2025-09-06 16:08:31 +0300

    finished the course

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,40 +1,13 @@
 import express from "express";
-import type { Request, Response } from "express";
 import dotenv from "dotenv";
-import OpenAI from "openai";
+import router from "./routes.ts";
 
 dotenv.config();
 
-const client = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY,
-});
-
 const app = express();
 app.use(express.json());
+app.use(router);
 const port = process.env.PORT || 3000;
-
-app.get("/", (req: Request, res: Response) => res.send("Hello world!"));
-app.get("/api/hello", (req: Request, res: Response) =>
-  res.send({ message: "Hello, World!" }),
-);
-
-const conversations = new Map<string, string>();
-
-app.post("/api/chat", async (req: Request, res: Response) => {
-  const { prompt, conversationId } = req.body;
-
-  const response = await client.responses.create({
-    model: "gpt-4o-mini",
-    input: prompt,
-    temperature: 0.2,
-    max_output_tokens: 100,
-    previous_response_id: conversations.get(conversationId),
-  });
-
-  conversations.set(conversationId, response.id);
-
-  res.json({ message: response.output_text });
-});
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
